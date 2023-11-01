5eec37f5efe2b057184e552eef5cbf19c6c28353
KostelBeats <67270862+KostelBeats@users.noreply.github.com>
2023-11-02 00:10:53 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module ru.teamentropy.irisrgb {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens ru.teamentropy.irisrgb to javafx.fxml;
+    exports ru.teamentropy.irisrgb;
+}
---
+++ src/main/java/ru/teamentropy/irisrgb/dataflow/DataFlowImpl.java
@@ -0,0 +1,2 @@
+package ru.teamentropy.irisrgb.dataflow;public class DataFlowImpl {
+}
